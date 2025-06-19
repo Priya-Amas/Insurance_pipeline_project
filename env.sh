e8571c2b4bf957f98fa000231c80eddee35d8683
Shunmuga Priya A <priya123@gmail.com>
2025-06-19 17:13:40 +0530

    airflow config

---
+++ env.sh
@@ -0,0 +1,4 @@
+# added by Snowflake SnowSQL installer
+export PATH=/home/priya/bin:$PATH
+export AIRFLOW_HOME=~/insurance/insurance_pipeline/airflow
+export AIRFLOW__CORE__DAGS_FOLDER=~/insurance/insurance_pipeline/dags
